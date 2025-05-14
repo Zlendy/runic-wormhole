31fadfcc33584813890e69d7451b505e990a1d4b
Zlendy <zlendy@proton.me>
2025-05-14 23:21:57 +0200

    style: Run prettier on all files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,61 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
-  import { getCurrentWindow } from '@tauri-apps/api/window';
-  import { Maximize, Minimize, Minus, X } from '@lucide/svelte';
-  import { Button } from "$lib/components/ui/button";
-  import { isMaximized } from "$lib/window.svelte";
+	import { invoke } from '@tauri-apps/api/core';
+	import { getCurrentWindow } from '@tauri-apps/api/window';
+	import { Maximize, Minimize, Minus, X } from '@lucide/svelte';
+	import { Button } from '$lib/components/ui/button';
+	import { isMaximized } from '$lib/window.svelte';
 
-  const appWindow = getCurrentWindow();
+	const appWindow = getCurrentWindow();
 
-  appWindow.onResized(async () => {
-    isMaximized.value = await appWindow.isMaximized();
-  });
+	appWindow.onResized(async () => {
+		isMaximized.value = await appWindow.isMaximized();
+	});
 
-  let name = $state("");
-  let greetMsg = $state("");
+	let name = $state('');
+	let greetMsg = $state('');
 
-  async function greet(event: Event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
-  }
+	async function greet(event: Event) {
+		event.preventDefault();
+		// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+		greetMsg = await invoke('greet', { name });
+	}
 </script>
 
-<div class="flex p-1 gap-1 flex-row-reverse" data-tauri-drag-region>
-  <Button class="focus-visible:ring-0 focus-visible:ring-offset-0 focus:bg-accent focus:text-accent-foreground" variant="ghost" size="icon" onclick={appWindow.close}>
-    <X />
-  </Button>
-  <Button class="focus-visible:ring-0 focus-visible:ring-offset-0 focus:bg-accent focus:text-accent-foreground" variant="ghost" size="icon" onclick={appWindow.toggleMaximize}>
-    {#if isMaximized.value}
-      <Minimize />
-    {:else}
-      <Maximize />
-    {/if}
-  </Button>
-  <Button class="focus-visible:ring-0 focus-visible:ring-offset-0 focus:bg-accent focus:text-accent-foreground" variant="ghost" size="icon" onclick={appWindow.minimize}>
-    <Minus />
-  </Button>
+<div class="flex flex-row-reverse gap-1 p-1" data-tauri-drag-region>
+	<Button
+		class="focus:bg-accent focus:text-accent-foreground focus-visible:ring-0 focus-visible:ring-offset-0"
+		variant="ghost"
+		size="icon"
+		onclick={appWindow.close}
+	>
+		<X />
+	</Button>
+	<Button
+		class="focus:bg-accent focus:text-accent-foreground focus-visible:ring-0 focus-visible:ring-offset-0"
+		variant="ghost"
+		size="icon"
+		onclick={appWindow.toggleMaximize}
+	>
+		{#if isMaximized.value}
+			<Minimize />
+		{:else}
+			<Maximize />
+		{/if}
+	</Button>
+	<Button
+		class="focus:bg-accent focus:text-accent-foreground focus-visible:ring-0 focus-visible:ring-offset-0"
+		variant="ghost"
+		size="icon"
+		onclick={appWindow.minimize}
+	>
+		<Minus />
+	</Button>
 </div>
 
 <main class="container">
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
-  <p>{greetMsg}</p>
+	<form class="row" onsubmit={greet}>
+		<input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+		<button type="submit">Greet</button>
+	</form>
+	<p>{greetMsg}</p>
 </main>
