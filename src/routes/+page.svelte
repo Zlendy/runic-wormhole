083c609bce819579c35b5bf885ef0f274e5a64d9
Zlendy <zlendy@proton.me>
2025-05-14 22:20:54 +0200

    feat: Create a custom titlebar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/core";
+  import { getCurrentWindow } from '@tauri-apps/api/window';
+  import { Maximize, Minimize, Minus, X } from '@lucide/svelte';
+
+  const appWindow = getCurrentWindow();
+
+  appWindow.onResized(async () => {
+    isMaximized = await appWindow.isMaximized();
+  });
 
   let name = $state("");
   let greetMsg = $state("");
+  let isMaximized = $state(false);
 
   async function greet(event: Event) {
     event.preventDefault();
@@ -10,6 +19,22 @@
     greetMsg = await invoke("greet", { name });
   }
 </script>
+
+<div class="p-4 flex gap-4 flex-row-reverse" data-tauri-drag-region>
+  <button onclick={appWindow.close}>
+    <X />
+  </button>
+  <button onclick={appWindow.toggleMaximize}>
+    {#if isMaximized}
+      <Minimize />
+    {:else}
+      <Maximize />
+    {/if}
+  </button>
+  <button onclick={appWindow.minimize}>
+    <Minus />
+  </button>
+</div>
 
 <main class="container">
   <form class="row" onsubmit={greet}>
