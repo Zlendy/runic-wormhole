6f126fd34b8642348221ad1a76c7612ca3c026b0
Zlendy <zlendy@proton.me>
2025-05-15 14:35:33 +0200

    refactor: Isolate titlebar controls into its own module

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,44 @@
 <script lang="ts">
-  import { isMaximized } from '$lib/window.svelte';
 	import '../app.css';
+	import TauriWindow from '$lib/tauri-window.svelte';
+
+	import { Maximize, Minimize, Minus, X } from '@lucide/svelte';
+	import { Button } from '$lib/components/ui/button';
+
 	let { children } = $props();
 </script>
 
-<div class="bg-background h-screen" class:rounded-lg={!isMaximized.value}>
+<div class="h-screen bg-background" class:rounded-lg={!TauriWindow.isMaximized}>
+	<div class="mb-4 flex flex-row-reverse gap-1 border-b p-1" data-tauri-drag-region>
+		<Button
+			class="focus:bg-accent focus:text-accent-foreground focus-visible:ring-0 focus-visible:ring-offset-0"
+			variant="ghost"
+			size="icon"
+			onclick={close}
+		>
+			<X />
+		</Button>
+		<Button
+			class="focus:bg-accent focus:text-accent-foreground focus-visible:ring-0 focus-visible:ring-offset-0"
+			variant="ghost"
+			size="icon"
+			onclick={TauriWindow.toggleMaximize}
+		>
+			{#if TauriWindow.isMaximized}
+				<Minimize />
+			{:else}
+				<Maximize />
+			{/if}
+		</Button>
+		<Button
+			class="focus:bg-accent focus:text-accent-foreground focus-visible:ring-0 focus-visible:ring-offset-0"
+			variant="ghost"
+			size="icon"
+			onclick={TauriWindow.minimize}
+		>
+			<Minus />
+		</Button>
+	</div>
+
 	{@render children()}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,5 @@
 <script lang="ts">
 	import { invoke } from '@tauri-apps/api/core';
-	import { getCurrentWindow } from '@tauri-apps/api/window';
-	import { Maximize, Minimize, Minus, X } from '@lucide/svelte';
-	import { Button } from '$lib/components/ui/button';
-	import { isMaximized } from '$lib/window.svelte';
-
-	const appWindow = getCurrentWindow();
-
-	appWindow.onResized(async () => {
-		isMaximized.value = await appWindow.isMaximized();
-	});
 
 	let name = $state('');
 	let greetMsg = $state('');
@@ -20,37 +10,6 @@
 		greetMsg = await invoke('greet', { name });
 	}
 </script>
-
-<div class="flex flex-row-reverse gap-1 p-1" data-tauri-drag-region>
-	<Button
-		class="focus:bg-accent focus:text-accent-foreground focus-visible:ring-0 focus-visible:ring-offset-0"
-		variant="ghost"
-		size="icon"
-		onclick={appWindow.close}
-	>
-		<X />
-	</Button>
-	<Button
-		class="focus:bg-accent focus:text-accent-foreground focus-visible:ring-0 focus-visible:ring-offset-0"
-		variant="ghost"
-		size="icon"
-		onclick={appWindow.toggleMaximize}
-	>
-		{#if isMaximized.value}
-			<Minimize />
-		{:else}
-			<Maximize />
-		{/if}
-	</Button>
-	<Button
-		class="focus:bg-accent focus:text-accent-foreground focus-visible:ring-0 focus-visible:ring-offset-0"
-		variant="ghost"
-		size="icon"
-		onclick={appWindow.minimize}
-	>
-		<Minus />
-	</Button>
-</div>
 
 <main class="container">
 	<form class="row" onsubmit={greet}>
