fd639476461748b0f693869a561beaeff9030344
Zlendy <zlendy@proton.me>
2025-06-03 14:56:32 +0200

    feat: Remove custom title bar
    It makes the app unusable when the user maximizes it on macOS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import TauriWindow from '$lib/tauri-window.svelte';
-
-	import { Maximize, Minimize, Minus, X } from '@lucide/svelte';
 	import { Button } from '$lib/components/ui/button';
 
 	interface Route {
@@ -36,47 +33,14 @@
 	{/each}
 {/snippet}
 
-<div class="h-screen bg-background" class:rounded-lg={!TauriWindow.isMaximized}>
-	<div class="mb-4 flex flex-row-reverse gap-1 border-b p-1" data-tauri-drag-region>
-		<Button
-			class="focus:bg-accent focus:text-accent-foreground focus-visible:ring-0 focus-visible:ring-offset-0"
-			variant="ghost"
-			size="icon"
-			onclick={TauriWindow.close}
-		>
-			<X />
-		</Button>
-		<Button
-			class="focus:bg-accent focus:text-accent-foreground focus-visible:ring-0 focus-visible:ring-offset-0"
-			variant="ghost"
-			size="icon"
-			onclick={TauriWindow.toggleMaximize}
-		>
-			{#if TauriWindow.isMaximized}
-				<Minimize />
-			{:else}
-				<Maximize />
-			{/if}
-		</Button>
-		<Button
-			class="focus:bg-accent focus:text-accent-foreground focus-visible:ring-0 focus-visible:ring-offset-0"
-			variant="ghost"
-			size="icon"
-			onclick={TauriWindow.minimize}
-		>
-			<Minus />
-		</Button>
-	</div>
+<header class="hidden flex-wrap justify-center gap-4 sm:flex mt-4">
+	{@render nav()}
+</header>
 
-	<header class="hidden flex-wrap justify-center gap-4 sm:flex">
-		{@render nav()}
-	</header>
+<main class="flex justify-center p-4">
+	{@render children()}
+</main>
 
-	<main class="flex justify-center p-4">
-		{@render children()}
-	</main>
-
-	<footer class="fixed bottom-4 left-0 right-0 flex flex-wrap justify-center gap-4 sm:hidden">
-		{@render nav()}
-	</footer>
-</div>
+<footer class="fixed bottom-4 left-0 right-0 flex flex-wrap justify-center gap-4 sm:hidden">
+	{@render nav()}
+</footer>
